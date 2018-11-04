2ca5c1d3638754a4177027e7fc70098179f844d1
xiechongyang7 <xiechongyang7@gmail.com>
2018-11-04 23:18:39 +0800

    添加

--- src/main/java/getImg/GetImg.java
+++ src/main/java/getImg/GetImg.java
@@ -6,8 +6,13 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 
 /**
  * @Description
@@ -18,26 +23,76 @@ import java.io.IOException;
 public class GetImg {
 
 
-    public static void main(String arg[]){
-        long a  = System.currentTimeMillis();
-        try {
-            String a1 = null;
-            for (int i = 2;i<5000;i++){
-                try {
-                    a1 = HttpConnectionPoolUtil.get("http://www.duokan.com/book/"+i);
-                    Document doc = Jsoup.parse(a1);
-                    System.out.println(doc.title());
-                }catch (Exception e){
-                    System.out.println(i+"出错了"+":::::::::::::::::::::::");
-                }
-            }
+    public static void main(String arg[]) {
+        Statement statement = null;
+        Connection conn = null;
 
+        try {
+            conn = ConnectionPool.getConnection();
+            //关键 设置为手动提交
+            conn.setAutoCommit(false);
+            statement = conn.createStatement();
         } catch (Exception e) {
             e.printStackTrace();
             System.out.println(e);
+            System.out.println("sql报错");
         }
-        long b  = System.currentTimeMillis();
-            System.out.println(b-a);
+        for (int i = 640; i < 10000; i++) {
+            try {
+                String a1 = HttpConnectionPoolUtil.get("http://www.duokan.com/book/" + i);
+                Document doc = Jsoup.parse(a1);
+                Elements e = doc.getElementsByTag("img");
+                for (int y = 2; y < e.size(); y++) {
+                    Element element = e.get(y);
+                    String name = element.attr("alt");
+                    String src = element.attr("src");
+                    if (!"".equals(name)) {
+                        StringBuilder builder = new StringBuilder("insert into department (dname,address) values(");
+                        builder.append("'");
+                        builder.append(name);
+                        builder.append("'");
+                        builder.append(",");
+                        builder.append("'");
+                        builder.append(src);
+                        builder.append("'");
+                        builder.append(")");
+                        System.out.println(builder);
+                        statement.addBatch(builder.toString());
+                    }
+                }
+            } catch (Exception e) {
+                System.out.println("第" + i + "获取出错了" + ":::::::::::::::::::::::");
+            }
+            if (i % 100 == 0) {
+                try {
+                    System.out.println("第" + i + "个提交");
+                    statement.executeBatch();
+                    conn.commit();
+                    System.out.println("提交完毕");
+                } catch (Exception e) {
+                    System.out.println("第" + i + "sql出错了::::::::::::::::::::::::::::::::::::::::::::");
+                }
+
+            }
+        }
+
+
+    }
+
+}
+/**
+ * //                    String title = doc.title();
+ * //        long a  = System.currentTimeMillis();
+ * <p>
+ * //            String sql = "insert into department (dname,address) values(?,?)";
+ * //            PreparedStatement pstmt;
+ * //            pstmt = conn.prepareStatement(sql);
+ * //                            pstmt.setString(1,name);
+ * //                            pstmt.setString(2,src);
+ *///                    System.out.println("出错了" + ":::::::::::::::::::::::");
+
+//        long b  = System.currentTimeMillis();
+//            System.out.println(b-a);
 //        Document doc = null;
 //        try {
 //            long a  = System.currentTimeMillis();
@@ -57,6 +112,11 @@ public class GetImg {
 //            e.printStackTrace();
 //        }
 //        String title = doc.title();
-    }
 
-}
+/**
+ * //            String sql = "insert into department (dname,address) values(?,?)";
+ * //            PreparedStatement pstmt;
+ * //            pstmt = conn.prepareStatement(sql);
+ *             //                            pstmt.setString(1,name);
+ * //                            pstmt.setString(2,src);
+ */
---
+++ src/main/java/getImg/JdbcPool.java
@@ -0,0 +1,12 @@
+package getImg;
+
+/**
+ * @Description
+ * @Since JDK1.8
+ * @Createtime 2018/11/4 下午 9:40
+ * @Author xiechongyang
+ */
+public class JdbcPool {
+
+
+}
